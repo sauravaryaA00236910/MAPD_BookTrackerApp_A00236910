9fc499233c759ebd6f6a50686b880670a726a477
sauravaryaA00236910 <84348791+sauravaryaA00236910@users.noreply.github.com>
2022-02-03 03:17:43 -0500

    login page layout added

--- app/src/main/java/com/saurav/mapd_booktrackerapp_a00236910/MainActivity.java
+++ app/src/main/java/com/saurav/mapd_booktrackerapp_a00236910/MainActivity.java
@@ -4,11 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.saurav.mapd_booktrackerapp_a00236910.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+//    view binding
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
     }
 }
--- app/src/main/java/com/saurav/mapd_booktrackerapp_a00236910/SplashActivity.java
+++ app/src/main/java/com/saurav/mapd_booktrackerapp_a00236910/SplashActivity.java
@@ -2,7 +2,9 @@ package com.saurav.mapd_booktrackerapp_a00236910;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -10,6 +12,16 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+//        start main screen after 2 seconds
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                finish();
+            }
+        }, 2000);
 
 
     }
