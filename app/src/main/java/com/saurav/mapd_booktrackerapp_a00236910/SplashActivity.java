c2386bf7fbf43b06be7356e5970b092c00f9c0f3
sauravaryaA00236910 <84348791+sauravaryaA00236910@users.noreply.github.com>
2022-02-03 02:24:52 -0500

    splash screen added

---
+++ app/src/main/java/com/saurav/mapd_booktrackerapp_a00236910/SplashActivity.java
@@ -0,0 +1,16 @@
+package com.saurav.mapd_booktrackerapp_a00236910;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+
+    }
+}
